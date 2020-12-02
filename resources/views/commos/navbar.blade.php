46608275d44f670111f73308bf9f684efbcfde21
梅木邦彦 <umekikunihiko@umekikunihikonoMacBook-Pro.local>
2020-12-02 11:58:31 +0900

    login

--- resources/views/commos/navbar.blade.php
+++
@@ -1,6 +0,0 @@
-<ul class="nav navbar-nav navbar-right">
-                    {{-- ユーザ登録ページへのリンク --}}
-                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-                    {{-- ログインページへのリンク --}}
-                    <li><a href="#">Login</a></li>
-                </ul>
